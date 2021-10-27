083c0faed5f9fcb39923c768938b901010d6bb4b
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-27 15:00:59 +0200

    lesson 8 final files
    using state - useState hook

--- raven-blog/src/Home.js
+++ raven-blog/src/Home.js
@@ -1,18 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
-        console.log('hello Raveners')
-    }
-
-    const handleClickAgain = name => {
-        console.log('hello ' + name)
+        setName('luigi');
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
         </div>
     );
 }
