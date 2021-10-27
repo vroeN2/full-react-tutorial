ac5b85e24976a95d4311c2681946aaeb295f88a2
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-27 07:59:04 +0200

    lesson 5 final files
    create component and import it to app.js

--- raven-blog/src/App.js
+++ raven-blog/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = 'http://www.google.com';
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ 'hello!' }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 40 }</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>    
     </div>
   );
