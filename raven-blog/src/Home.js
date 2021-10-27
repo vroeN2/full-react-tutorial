3494514ae0aa3ed115dca1c68dc557e9bc318a7e
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-27 11:50:48 +0200

    lesson 7 final files
    Click events, pass events, decide when the function should invoke and how to avoid unwanted invocations

--- raven-blog/src/Home.js
+++ raven-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello Raveners')
+    }
+
+    const handleClickAgain = name => {
+        console.log('hello ' + name)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
         </div>
     );
 }
