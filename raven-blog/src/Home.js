c6047b1318670e3c12a87693b5a7fc5e027d9125
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-29 05:39:51 +0200

    lesson 12 final files
    reuse component in a different way, filter data passed as a props

--- raven-blog/src/Home.js
+++ raven-blog/src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title='All Blogs!' /> 
+            <BlogList blogs={blogs.filter(blogs => blogs.author === 'mario')} title="Mario's blogs!" /> 
         </div>
     );
 }
