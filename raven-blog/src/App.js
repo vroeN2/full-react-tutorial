63847cf1b045029b4eab1569c9b8e3e6f5ff42c9
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-30 09:51:25 +0200

    lesson 32 final files
    create 404 page for all non-existing links

--- raven-blog/src/App.js
+++ raven-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -21,6 +22,10 @@ function App() {
             {/* ':id' is used as a placeholder implicating that there will be x amount of sublinks from /blogs. 'id' is just a name, for easier recognition, but it can be anything else */}
             <Route path="/blogs/:id" >
               <BlogDetails />
+            </Route>
+            {/* '*' mathes ANY string. This goes to the very bottom of the routing section, because it would match any link, thus some pages would not be displayed as intended */}
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>    
