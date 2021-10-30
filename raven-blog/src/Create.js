7a1d580fe7c8436fab09e1775735f31c336dd6e6
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-30 09:12:29 +0200

    lesson 27 final files
    create and control form for creating a new blog

--- raven-blog/src/Create.js
+++ raven-blog/src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required 
+                    // each field of this form is controlled - it's value is stored in state, each has onChange function that updates the value with no downtime, and it is immediately shown on the position
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author</label>
+                <select
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
     );
 }
