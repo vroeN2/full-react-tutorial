f8cf4fe14ece62c9c1361ece0e14c3b6e0169baa
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-30 09:42:56 +0200

    lesson 31 final files
    delete blogs, redirect user to the main page after that

--- raven-blog/src/BlogDetails.js
+++ raven-blog/src/BlogDetails.js
@@ -1,10 +1,22 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
     // fetching blog details for clicked item 
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    //redirection to the homepage again - with useHistory!
+    const history = useHistory();
+
+    const handleClick = () => {
+        // delete opened blog post from the db
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            // push user to the homepage
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -18,6 +30,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
